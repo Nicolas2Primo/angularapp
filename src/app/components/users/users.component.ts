e4fba04f975f50703c0763de584d63354466bc92
Nicolas Primo <nicolasprimo@gmail.com>
2023-04-12 14:48:57 -0300

    Error

---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/User';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css'],
+})
+export class UsersComponent implements OnInit {
+  users: User[] = [];
+  showExtended: boolean = true;
+  loaded: boolean = false;
+
+  constructor() {
+    console.log(this.users);
+  }
+
+  ngOnInit(): void {
+    this.users = [
+      {
+        firstName: 'Maria',
+        lastName: 'Leticia',
+        age: 21,
+        address: {
+          street: '50 Main st',
+          city: 'Boston',
+          state: 'MA',
+        },
+      },
+      {
+        firstName: 'Nicolas',
+        lastName: 'Primo',
+        age: 20,
+        address: {
+          street: '50 Main st',
+          city: 'Boston',
+          state: 'MA',
+        },
+      },
+      {
+        firstName: 'Willian',
+        lastName: 'Fialho',
+        age: 19,
+        address: {
+          street: 'Oliveira paiva',
+          city: 'Fortaleza',
+          state: 'CE',
+        },
+      },
+    ];
+
+    this.loaded = true;
+  }
+
+  addUser(user: User) {
+    this.users.push(user);
+  }
+}
